682a44d8f47afceff368b0a424f8cdad7e058133
Nruzhzhaugash <clonn262@gmail.com>
2024-02-13 00:24:25 +0600

    fix

--- src/features/Filter/ui/Filter.tsx
+++ src/features/Filter/ui/Filter.tsx
@@ -3,17 +3,17 @@ import './Filter.scss';
 
 export const Filter = () => {
   const data = [
-    { name: 'Двери и окна' },
-    { name: 'Стены и фасады' },
-    { name: 'Техническое обслуживание и Ремонт' },
+    { name: 'Двери и окна',  },
+    { name: 'Стены и фасады', },
+    { name: 'Техническое обслуживание и Ремонт', className: 'checkbox__filter w-[39px] h-[33px] mr-2.5' },
   ]
   
   return (
     <ul className="">
-      {data.map(({ name }, index ) => (
+      {data.map(({ name, className }, index ) => (
         <li key={index} className="py-2.5 pr-2.5 flex mb-2.5">
-          <Input type="checkbox" className="mr-2.5 checkbox__filter flex"></Input>
-          <h3 className="checbox__text text-left">{name}</h3>
+          <Input type="checkbox" className={`${name === 'Техническое обслуживание и Ремонт' ? className : 'checkbox__filter w-[33px] h-[33px] mr-2.5 flex'}`} />
+          <h3 className="checbox__text items-center">{name}</h3>
         </li>
       ))}
     </ul>
